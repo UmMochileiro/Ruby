b738cad32148bda9d54e8b3dda43133aee2cd850
Matheus <matheus2cardoso@icloud.com>
2025-02-22 19:28:43 -0300

    Add a simple Ruby program for user interaction

---
+++ Hello.rb
@@ -0,0 +1,13 @@
+puts "Hey There! Welcome to Ruby Programming, here i'll learn and create some stuff on ruby programming language."
+puts "Let's get started with the basics of Ruby Programming Language."
+puts "Qual seu nome?"
+
+nome = gets.chomp
+
+puts "Nice to meet you #{nome}"
+puts "What is your age?"
+
+idade = gets.chomp.to_i
+
+puts "Wow! You are #{idade} years old. That's great!"
+
