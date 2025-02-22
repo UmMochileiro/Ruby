1d77f31291962c628f76d7e83ec6d368747a2e93
Matheus <matheus2cardoso@icloud.com>
2025-02-22 19:55:01 -0300

    hello

---
+++ RubyRepository/Hello.rb
@@ -0,0 +1,22 @@
+puts "Hey There! Welcome to Ruby Programming, here i'll learn and create some stuff on ruby programming language."
+puts "Let's get started with the basics of Ruby Programming Language."
+puts "Qual seu nome?"
+
+nome = gets.chomp
+
+puts "Nice to meet you #{nome}"
+puts "What is your age?"
+
+idade = gets.chomp.to_i
+
+puts "Wow! You are #{idade} years old. That's great!"
+
+puts "What is your favorite color?"
+favcolor = gets.chomp
+
+puts "Nice! #{favcolor} is a great color."
+
+puts "What is your favorite team?"
+favteam = gets.chomp
+
+puts "Nice! #{favteam} is a great team."
